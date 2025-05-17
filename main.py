ae73e34260b04106137dfa9c1926f796e119d5ce
Yashi Ary <yashi.vaidehi.arya@gmail.com>
2025-05-17 00:15:30 -0400

    initial

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
-# This is a sample Python script.
+import discord
+import logging
+import logging.handlers
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+intents = discord.Intents.all()
+##intents.members = True
+##intents.voice_states = True
+##intents.messages = True
+
+julieUserId = 879809989189980161
+gabeUserId = 612385958217908264
+wallyUserId = 480772749518831617
+vcTextChannelId = 1327813409852362794
+token = 'MTM3MzEyNjYyOTI1NzM3OTg2MA.G8zu5C.DfQ9l7jJU6PH-8c1IHsvOEk4n6cDTKsFhJ_OVk'
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!",intents=discord.Intents.default()) # prefix is the bot command
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    channel = bot.get_channel(vcTextChannelId)
+    # Only act when the member leaves a voice channel
+    if before.channel is not None and after.channel is None:
+        if member.id in [julieUserId, gabeUserId]:
+            name_upper = member.display_name.upper()
+
+            embed = discord.Embed(
+                title="Did you enjoy VC? We know you did ;)",
+                description=f"**{name_upper} OUT**\n\n*Leave a TIP!*",
+                color=discord.Color.purple()
+            )
+            embed.set_image(url="attachment://TipJar.png")  # This will be the uploaded file
+            embed.set_footer(text=f"ID: {member.id} • {discord.utils.format_dt(discord.utils.utcnow(), style='t')}")
+            embed.set_author(name=member.name, icon_url=member.avatar.url if member.avatar else None)
+
+            # Attach image file
+            file = discord.File(r"D:\Downloads\TipJar.png", filename="TipJar.png")
+            await channel.send(embed=embed, file=file)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
 
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Assume client refers to a discord.Client subclass...
+# Suppress the default configuration since we have our own
+bot.run(token, log_handler=None)
